c5edecd11a1dbf6f5a853104096cf992e76e534c
joe thaxton <14jthaxton@gmail.com>
2023-09-02 11:49:28 -0700

    First commit

--- src/stories/Avatar.jsx
+++ src/stories/Avatar.jsx
@@ -3,7 +3,6 @@ import {default as MUIAvatar} from '@mui/material/Avatar';
 
 function Avatar({alt, src, loading}) {
     const avatar = <MUIAvatar width={40} height={40} variant="circular" alt={alt} src={src} />;
-    
     return (
         <>
             {(loading) ? 
